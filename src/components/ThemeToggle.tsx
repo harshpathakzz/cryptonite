788c2797e7072984b931fa4d3dcf3eef27063688
harshpathakzz <harshpathak2017@gmail.com>
2024-07-18 11:20:32 +0530

    added theme

---
+++ src/components/ThemeToggle.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { useThemeStore } from "@/store/themeStore";
+import { Sun, Moon } from "lucide-react";
+
+export const ThemeToggle = () => {
+  const { theme, toggleTheme } = useThemeStore();
+
+  return (
+    <button
+      onClick={toggleTheme}
+      className="p-2 rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+    >
+      {theme === "light" ? <Moon /> : <Sun />}
+    </button>
+  );
+};
