707f68e4a9f327159f0cc3e80212eb27454d419a
harshpathakzz <harshpathak2017@gmail.com>
2024-07-19 11:31:25 +0530

    added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,44 +26,7 @@ export default function Home() {
         laudantium hic neque saepe molestias. Ratione perspiciatis reprehenderit
         placeat alias fugit cum labore. Et pariatur est iusto omnis quas a ex,
         rem reprehenderit, corporis rerum explicabo provident eligendi aliquid,
-        totam eos tempora alias soluta aspernatur numquam! Vitae rem saepe
-        cupiditate fugit praesentium consequatur libero, blanditiis minima
-        laboriosam obcaecati harum qui neque nostrum aspernatur reprehenderit
-        aut! Deleniti esse sint quaerat mollitia? Nostrum tempore placeat hic
-        debitis, incidunt sapiente. Dolorum, deleniti veniam. Officia suscipit
-        amet iure architecto provident vero esse doloremque minus, corporis
-        veniam rem alias aut voluptas ducimus neque, perspiciatis beatae fugit.
-        Aliquid, quidem ullam? Alias corporis, cum illo dignissimos commodi est
-        non omnis porro veritatis quae nemo recusandae numquam officia explicabo
-        quis natus. Asperiores, adipisci voluptates quae quam ipsum accusamus
-        incidunt doloremque, illum deleniti a maiores laborum corrupti eius,
-        nisi tempore ipsam nulla facilis! Repellat perspiciatis consequatur at
-        dicta earum, recusandae libero! Enim molestiae, harum ipsa repellendus
-        iure porro assumenda perspiciatis at deserunt corrupti, vel dolor!
-        Aliquid, tempora adipisci? Modi culpa hic laboriosam necessitatibus fuga
-        optio amet, quos voluptatum expedita nesciunt nostrum quia nihil
-        molestias? Numquam, tempora quisquam reiciendis impedit exercitationem
-        et dolores repudiandae error asperiores adipisci, distinctio
-        reprehenderit, nihil cum eveniet architecto. Excepturi perspiciatis
-        tenetur quod, ut commodi eum labore maiores aspernatur saepe! Eveniet
-        quaerat autem porro veritatis minus cupiditate velit distinctio impedit
-        quidem, voluptate quos asperiores pariatur repellendus ipsa, ducimus
-        delectus beatae ullam repellat. Eveniet praesentium provident nobis
-        atque reiciendis iste quis ut sunt error aperiam. Similique fugiat odio
-        explicabo illo neque magni ea nisi numquam tempora cupiditate aut
-        sapiente voluptatem exercitationem provident est magnam, culpa quod
-        labore doloribus. Aperiam amet dolores maiores enim nemo illo impedit
-        maxime fugit velit natus nesciunt rem animi, libero molestiae magni. Eos
-        illo magnam placeat saepe reiciendis cum, facilis porro corporis ipsam
-        eius aut ad ex nam provident vitae repudiandae. Aperiam rem at libero
-        molestias iste! Dolorem vero totam provident quos id blanditiis
-        voluptatem temporibus enim facilis rem suscipit, odit qui adipisci
-        voluptate quia nostrum assumenda officia fugiat beatae cum laboriosam?
-        Impedit, nostrum accusamus? Consequuntur ipsum deserunt distinctio at
-        quod accusamus, laboriosam quam. Vel, doloremque? Nostrum iure, aut ad
-        ipsa et commodi.
       </div>
-      <SearchBar />
     </>
   );
 }
