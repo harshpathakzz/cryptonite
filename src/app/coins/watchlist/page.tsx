81b25d0c7a75e0321e9cfc9847864e72baf22182
harshpathakzz <harshpathak2017@gmail.com>
2024-07-21 13:59:44 +0530

    added skeleton

--- src/app/coins/watchlist/page.tsx
+++ src/app/coins/watchlist/page.tsx
@@ -5,40 +5,49 @@ import { useWatchlistStore } from "@/store/watchListStore";
 import { getWatchListCoins } from "@/lib/api";
 import Link from "next/link";
 import WatchListCoinCard from "@/components/WatchListCoinCard";
+import { useErrorHandling } from "@/hooks/useErrorHandling";
+import { Skeleton } from "@/components/Skeleton";
 
 const WatchListPage = () => {
   const watchlist = useWatchlistStore((state) => state.watchlist);
   const watchlistIds = watchlist.map((item) => item.id);
-
-  const { isLoading, error, data } = useQuery({
+  const { isLoading, error, data, isError } = useQuery({
     queryKey: ["exploreCoins", watchlistIds],
     queryFn: () => getWatchListCoins(watchlistIds),
   });
 
-  if (isLoading) return <div className="text-center mt-8">Loading...</div>;
-  if (error)
-    return (
-      <div className="text-center mt-8 text-red-500">
-        Error: {error.message}
-      </div>
-    );
+  useErrorHandling(isError, error);
+
+  if (error) {
+    return null;
+  }
 
   return (
     <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-8 text-center">Your Watchlist</h1>
+      <h1 className="text-3xl font-bold mb-8 text-left">Your Watchlist</h1>
       {watchlist.length === 0 ? (
-        <p className="text-center text-lg text-gray-600 dark:text-gray-400">
+        <p className=" text-lg text-accent-foreground  text-left">
           Your watchlist is empty. Add some coins to track!
         </p>
       ) : (
-        <div className="space-y-6 ">
-          {data?.map((coin: any) => (
-            <Link href={`/coins/${coin.id}`} key={coin.id} className="block">
-              <div className="">
-                <WatchListCoinCard coin={coin} />
-              </div>
-            </Link>
-          ))}
+        <div className="space-y-6">
+          {isLoading
+            ? Array(3)
+                .fill(0)
+                .map((_, index) => (
+                  <Skeleton key={index} className="h-24 w-full" />
+                ))
+            : data?.map((coin: any) => (
+                <Link
+                  href={`/coins/${coin.id}`}
+                  key={coin.id}
+                  className="block"
+                >
+                  <div className="">
+                    <WatchListCoinCard coin={coin} />
+                  </div>
+                </Link>
+              ))}
         </div>
       )}
     </div>
