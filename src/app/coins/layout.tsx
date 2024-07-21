a5dd695f0899f2de4a7da7b713c9607903317f55
harshpathakzz <harshpathak2017@gmail.com>
2024-07-21 21:14:48 +0530

    updated

--- src/app/coins/layout.tsx
+++ src/app/coins/layout.tsx
@@ -8,10 +8,10 @@ export default function CoinsLayout({
   children: React.ReactNode;
 }) {
   return (
-    <section className="min-h-screen flex flex-col">
+    <section className="min-h-screen flex flex-col mb-10">
       <Header />
       <div className="flex">
-        <div className="w-full p-4 sm:w-2/3">{children}</div>
+        <div className="w-full p-4 sm:w-2/3 mb-10">{children}</div>
         <div className="w-1/3 p-4 hidden sm:block ">
           <div className="sticky top-24 right-5">
             <WatchList />
